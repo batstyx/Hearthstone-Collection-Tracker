5ec9c82be3e5f97463e0072d1145bd5407704166
Batian Styx <batstyx@gmail.com>
2020-04-18 10:59:40 +0100

    Tidy solution

--- Hearthstone Collection Tracker/Internal/Helpers.cs
+++ Hearthstone Collection Tracker/Internal/Helpers.cs
@@ -1,5 +1,4 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Drawing;
 using System.Net;
 using System.Text;
@@ -19,28 +18,6 @@ namespace Hearthstone_Collection_Tracker.Internal
             return source.IndexOf(toCheck, comp) >= 0;
         }
 
-        public static async Task<Version> GetLatestVersion()
-        {
-            const string latestReleaseRequestUrl =
-                @"https://api.github.com/repos/ko-vasilev/Hearthstone-Collection-Tracker/releases/latest";
-
-            try
-            {
-                string versionStr;
-                using (var wc = new WebClient())
-                {
-                    wc.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
-                    versionStr = await wc.DownloadStringTaskAsync(latestReleaseRequestUrl);
-                }
-                var versionObj = JsonConvert.DeserializeObject<GithubRelease>(versionStr);
-                return versionObj == null ? null : new Version(versionObj.TagName);
-            }
-            catch (Exception)
-            {
-            }
-            return null;
-        }
-
         public static string GetValidFileName(this string fileName)
         {
             StringBuilder initialString = new StringBuilder(fileName);
@@ -49,12 +26,6 @@ namespace Hearthstone_Collection_Tracker.Internal
                 initialString = initialString.Replace(c, '_');
             }
             return initialString.ToString();
-        }
-
-        private class GithubRelease
-        {
-            [JsonProperty("tag_name")]
-            public string TagName { get; set; }
         }
 
         public static double GetAvgHue(this Bitmap bmp, double saturationThreshold = 0.05)
--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,27 +1,3 @@
-if (Test-Path "nuget.exe") {
-	Invoke-Expression "./nuget restore"
-}
-elseif((Get-Command "nuget" -ErrorAction SilentlyContinue) -ne $null) {
-	nuget restore
-}
-else {
-	"Nuget was not found and is required to run bootstrap.ps. Download and retry now?"
-	choice /c yn
-	if ($LASTEXITCODE -eq 1) {
-		"Downloading nuget..."
-		$nugetLocation = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
-		(New-Object Net.WebClient).DownloadFile($nugetLocation, "$PSScriptRoot\nuget.exe")
-		Invoke-Expression "./nuget restore"
-	}
-}﻿
- 
- function FetchLib($name) {
-	"Fetching $name..."
-	$url = "https://libs.hearthsim.net/hdt/$name.dll"
-	try { (New-Object Net.WebClient).DownloadFile($url, "$PSScriptRoot\lib\$name.dll") }
-    catch { $error[0].Exception.ToString() }
-}
-
 md -Force $PSScriptRoot\lib | Out-Null
 
 FetchLib "HearthDb"
